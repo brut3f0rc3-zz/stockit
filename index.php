47580714d589d7b1d515e9a51921fcba95c0f828
brut3f0rc3 <palash.s.chatterjee@gmail.com>
2014-10-26 18:00:32 +0530

    Initial commit

--- index.php
+++ index.php
@@ -9,14 +9,26 @@
 	<!--Fonts-->
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Alegreya+Sans+SC' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Kalam' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Jim+Nightshade' rel='stylesheet' type='text/css'>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript">
+$(window).load(function() {
+	$(".loader").fadeOut("slow");
+})
+</script>
 </head>
 <body>
+	<div class="loader"></div>
 	<nav>
 		<div id="nav-left">
 			<!--Stock It Logo-->
 		</div>
 
 		<div id="nav-right">
+
 			<input type="text" placeholder="john@doe.in">
 			<input type="text" placeholder="password">
 			<button class="btn btn-default">Login</button>
@@ -30,7 +42,9 @@
 		<div id="slide2" class="scrollable">
 			<div id="slide-text" style="float: left">
 				<article>
-					<blockquote style="font-family: 'Raleway', sans-serif">One of the funny things about the stock market is that every 	time one person buys, another sells, and both think they are astute.<br/>William Feather</blockquote>
+					<blockquote style="font-family: 'Raleway', sans-serif">
+					<i class="fa fa-quote-left fa-3x pull-left fa-border"></i>
+					One of the funny things about the stock market is that every 	time one person buys, another sells, and both think they are astute.<br/>William Feather</blockquote>
 					<p>A virtual, stock-market based game that will carry you the fathomless depths and inaccessible heights of the world of stock market.</p>
 					<p> Do you have it in you to join this roller-coaster ride?</p>
 					<p>Think before you leap... leap before it leaves!</p>
@@ -43,17 +57,38 @@
 		</div>
 		<div id="slide3"></div>
 		<div id="slide4" class="scrollable">
-			<article>
-				
-			</article>
+			<div style="padding: 50px 20px;">
+				<article>
+					<div>
+						<p style="text-align: center; font-family: 'Alegreya Sans SC', sans-serif; font-variant: small-caps; font-size:2em">Contact Us</p>
+						<div style="margin: 0px auto">
+							<div class="contact" style="float:left">
+								<img src="http://graph.facebook.com/palash.chatterjee/picture?type=square&height=200&width=200" class="thumb-circle">
+								<div class="contact-name">
+									<p class="contact-name-main">Palash Chatterjee</p>
+									<p><a href="http://facebook.com/palash.chatterjee"><i class ="fa fa-facebook" style="padding:5px"></i>palash.chatterjee</a></p>
+								</div>
+							</div>
+							<div class="contact" style="float:right">
+								<img src="http://graph.facebook.com/rohan.sahay/picture?type=square&height=200&width=200" class="thumb-circle">
+								<div class="contact-name">
+									<p class="contact-name-main">Rohan Sahay</p>
+									<p><a href="http://facebook.com/rohan.sahay"><i class ="fa fa-facebook" style="padding:5px"></i>rohan.sahay</a></p>
+								</div>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+					</div>
+				</article>
+			</div>
 		</div>
 	</div>
 	<footer>
 		&copy; <a href="http://geekonix.org">Geekonix</a>. 2014. All rights reserved.
 		<ul>
-			<li><a href="http://facebook.com/stockit"><i class ="fa fa-facebook-square fa-lg"></i></a></li>
-			<li><a href="http://twitter.com/stockit"><i class ="fa fa-twitter-square fa-lg"></i></a></li>
-			<li><a href="http://github.com/"><i class ="fa fa-github-square fa-lg"></i></a></li>
+			<li><a href="http://facebook.com/stockit"><i class="fa fa-facebook-square fa-lg"></i></a></li>
+			<li><a href="http://twitter.com/stockit"><i class="fa fa-twitter-square fa-lg"></i></a></li>
+			<li><a href="http://github.com/brut3f0rc3/stockit"><i class ="fa fa-github-square fa-lg"></i></a></li>
 		</ul>
 	</footer>
 	<script type="text/javascript">
