255e151c0d57752abe5db53891afc7eb0d19c369
brut3f0rc3 <palash.s.chatterjee@gmail.com>
2014-10-26 16:09:42 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>StockIt</title>
+	<!--CSS-->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="fa/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<!--Fonts-->
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<nav>
+		<div id="nav-left">
+			<!--Stock It Logo-->
+		</div>
+
+		<div id="nav-right">
+			<input type="text" placeholder="john@doe.in">
+			<input type="text" placeholder="password">
+			<button class="btn btn-default">Login</button>
+			<button class="btn btn-default">Register</button>
+		</div>
+		<div id="clearfix"></div>
+		
+	</nav>
+	<div id="body">
+		<div id="slide1"></div>
+		<div id="slide2" class="scrollable">
+			<div id="slide-text" style="float: left">
+				<article>
+					<blockquote style="font-family: 'Raleway', sans-serif">One of the funny things about the stock market is that every 	time one person buys, another sells, and both think they are astute.<br/>William Feather</blockquote>
+					<p>A virtual, stock-market based game that will carry you the fathomless depths and inaccessible heights of the world of stock market.</p>
+					<p> Do you have it in you to join this roller-coaster ride?</p>
+					<p>Think before you leap... leap before it leaves!</p>
+				</article>
+			</div>
+			<div id="slide-image" style="float:right">
+				<img src="images/stock3.png" height="70%" width="70%">
+			</div>
+			<div id="clearfix"></div>
+		</div>
+		<div id="slide3"></div>
+		<div id="slide4" class="scrollable">
+			<article>
+				
+			</article>
+		</div>
+	</div>
+	<footer>
+		&copy; <a href="http://geekonix.org">Geekonix</a>. 2014. All rights reserved.
+		<ul>
+			<li><a href="http://facebook.com/stockit"><i class ="fa fa-facebook-square fa-lg"></i></a></li>
+			<li><a href="http://twitter.com/stockit"><i class ="fa fa-twitter-square fa-lg"></i></a></li>
+			<li><a href="http://github.com/"><i class ="fa fa-github-square fa-lg"></i></a></li>
+		</ul>
+	</footer>
+	<script type="text/javascript">
+		//Login and registration scripts here
+	</script>	
+</body>
+</html>
